a5f4003ca2011fb1eb816920f034becea210c682
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-09-11 16:28:52 -0600

    Update app.module.ts

--- BumpBlog/src/app/app.module.ts
+++ BumpBlog/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './admin/home/home.component';
-
+import {MatSidenavModule} from '@angular/material/sidenav';
 @NgModule({
   declarations: [
     AppComponent,
